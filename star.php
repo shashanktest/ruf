581b483cc6f8f6fdc574fc112f58c5554e1ac637
shashank <shasha@gmail.com>
2016-06-03 09:54:04 +0530

    change

--- star.php
+++ star.php
@@ -1,4 +1,4 @@
-<?php /*
+<?php 
 for($i=0;$i<=5;$i++){
 	for($j=5-$i;$j>=1;$j--){
 		echo "*&nbsp;";
@@ -14,7 +14,7 @@ for($i=0;$i<=5;$i++){
 		echo "*&nbsp";
 	}
 	echo "</br>";
-}*/
+}
 ?>
 
 <?php /* 
